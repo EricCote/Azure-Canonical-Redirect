97d9f9de45440b7dea1ab13a0c72d446a468f32d
Eric Cote <eric@coteexpert>
2022-09-20 18:16:16 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const site = process.env.APEX_SITE || "https://www.reactacademy.live";
+
+app.get("*", (req, res) => {
+  res.redirect(site + req.path);
+});
+
+app.listen(port, () => {
+  console.log(`started at port ${port}`);
+});
